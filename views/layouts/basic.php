8d1295f9fac9650b1ab863b1c5bb18531152cc89
gooooodok <viktor_88@ukr.net>
2017-06-30 10:41:21 +0300

    Napolnenie svoegj shablona(Наполнение шаблона)

--- views/layouts/basic.php
+++ views/layouts/basic.php
@@ -1,10 +1,28 @@
+<?php
+
+use app\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+
+<?php $this->beginPage() ?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
+
+	<!-- Делаем адаптивный шаблон -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<title>Document</title>
+	<?php $this->head() ?>
 </head>
 <body>
+	<?php $this->beginBody() ?>
 	<h1>Hello, Basic</h1>
+
+	<?php $this->endBody() ?>
 </body>
 </html>
+<?php $this->endPage() ?>
