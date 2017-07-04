f7f6fc8ab50ee0cbadcf05b49a6304a34e5bdf48
gooooodok <viktor_88@ukr.net>
2017-07-04 14:42:00 +0300

    Validacia i Leyblu

--- models/TestForm.php
+++ models/TestForm.php
@@ -9,4 +9,21 @@ class TestForm extends Model
 	public $name;
 	public $email;
 	public $text;
+
+	public function attributeLabels ()
+	{
+		return [
+			'name' => 'Имя',
+			'email' => 'E-mail',
+			'text' => 'Сообщение'
+		];
+	}
+
+	public function rules()
+	{
+		return [
+			[ 'name', 'required', 'message' => 'Поле обязательно для заполнения' ],
+			[ 'email', 'required', 'message' => 'Поле обязательно для заполнения' ],
+		];
+	}
 }
